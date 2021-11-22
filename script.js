d11eeec3286adc163ff5cb6501a2ba5aba1650bc
JesusAGP <86432373+JesusAGP@users.noreply.github.com>
2021-11-21 21:23:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const dollarContainer = document.getElementById('dollar');
 const usdAmount = document.getElementById('usd-amount');
+var totalUsd;
 
 fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
     .then(response => response.json())
@@ -7,5 +8,12 @@ fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
 
 const displayData = data => {
     const usd = data.bpi.USD.rate_float;
+    totalUsd = usd;
     usdAmount.textContent = `$${usd} USD`;
+}
+
+function myFunction() {
+    var x = document.getElementById("fname").value;
+    var compra = x/totalUsd;
+    document.getElementById("demo").innerHTML = compra;
 }
